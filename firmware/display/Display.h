9510673f4cbb58ba50b482c74f4ef47ed0de0233
Nava Whiteford <new@sgenomics.co.uk>
2012-06-25 17:25:07 +0100

    Display an image...

--- firmware/display/Display.h
+++ firmware/display/Display.h
@@ -1,5 +1,7 @@
 #include "oled.h"
 
+extern uint8_t _binary_image_data_start;
+extern uint8_t _binary_image_data_size;
 
 class Display {
 
@@ -14,10 +16,17 @@ public:
 
   void test() {
 
+    dump_image();
+ /*
     uint8 data[100];
     for(int n=0;n<100;n++) data[n]=n;
 
     oled_draw_rect(10,10,20,20,data);
+*/
+  }
+
+  void dump_image() {
+    oled_draw_rect(0,0,128,128,(uint8_t *) &_binary_image_data_start);
   }
 
 };
--- firmware/display/oled.h
+++ firmware/display/oled.h
@@ -78,55 +78,55 @@
 #endif
 
 #define OLED_END 	(unsigned int)127
-#define GRAPH_START	(unsigned int)45
-#define GRAPH_END	(unsigned int)115
-#define GRAPH_RIGHT	(unsigned int)127
-#define GRAPH_LEFT	(unsigned int)0
-#define TIME_HEIGHT	(unsigned int)120
-#define TEXT_OFFSET	(unsigned int)5
-#define TEXT_SPACING	(unsigned int)4
-#define VAL_OFFSET	(unsigned int)90
-#define TEXT_HEIGHT	(unsigned int)45
+//#define GRAPH_START	(unsigned int)45
+//#define GRAPH_END	(unsigned int)115
+//#define GRAPH_RIGHT	(unsigned int)127
+//#define GRAPH_LEFT	(unsigned int)0
+//#define TIME_HEIGHT	(unsigned int)120
+//#define TEXT_OFFSET	(unsigned int)5
+//#define TEXT_SPACING	(unsigned int)4
+//#define VAL_OFFSET	(unsigned int)90
+//#define TEXT_HEIGHT	(unsigned int)45
 
 
 #define MAX_VARIABLES		11
 #define NUM_MEASUREMENTS	4
 
-#define RST_INDEX			4
-#define PONG_INDEX			5
-#define CUBE_INDEX			6
-#define GRAPH_INDEX			7
-#define LOG_INDEX			8
-#define CLOCK_INDEX			9
-#define PWR_INDEX			10
+//#define RST_INDEX			4
+//#define PONG_INDEX			5
+//#define CUBE_INDEX			6
+//#define GRAPH_INDEX			7
+//#define LOG_INDEX			8
+//#define CLOCK_INDEX			9
+//#define PWR_INDEX			10
 
 
-#define DAY_SIZE		(unsigned char)24
-#define WEEK_SIZE		(unsigned char)42
-#define MONTH_SIZE		(unsigned char)32
-#define YEAR_SIZE		(unsigned char)52
+//#define DAY_SIZE		(unsigned char)24
+//#define WEEK_SIZE		(unsigned char)42
+//#define MONTH_SIZE		(unsigned char)32
+//#define YEAR_SIZE		(unsigned char)52
 
 
-#define DOWN		0
-#define UP			1
+//#define DOWN		0
+//#define UP			1
 
-#define PADDLE_THICKNESS	(int)0x02
-#define PADDLE_HEIGHT		(int)0x18
-#define PADDLE_X_OFFSET		(int)0x02
-#define PADDLE_START		((int)OLED_END-PADDLE_THICKNESS)/2
-#define BALL_X_START		(int)60
-#define BALL_Y_START		(int)60
-#define BALL_THICKNESS		(int)5
+//#define PADDLE_THICKNESS	(int)0x02
+//#define PADDLE_HEIGHT		(int)0x18
+//#define PADDLE_X_OFFSET		(int)0x02
+//#define PADDLE_START		((int)OLED_END-PADDLE_THICKNESS)/2
+//#define BALL_X_START		(int)60
+//#define BALL_Y_START		(int)60
+//#define BALL_THICKNESS		(int)5
 
-#define PONG_TOP			(int)15
-#define PONG_SCORE_TOP		(int)(PONG_TOP - 12)
+//#define PONG_TOP			(int)15
+//#define PONG_SCORE_TOP		(int)(PONG_TOP - 12)
 
-#define COMPUTER	0
-#define PLAYER		1
+//#define COMPUTER	0
+//#define PLAYER		1
 
-#define PLAYER_MAX_SPD		(int)0x07
-#define COMPUTER_MAX_SPD	(int)0x05
-#define BALL_MAX_SPD		(int)0x0B
+//#define PLAYER_MAX_SPD		(int)0x07
+//#define COMPUTER_MAX_SPD	(int)0x05
+//#define BALL_MAX_SPD		(int)0x0B
 
 // convenience macros for OLED
 #define RGB16(r, b, g) ((((r)<<11L)&0x1fL) | (((g)<<5L)&0x3fL) | (((b)<<0L)&0x1fL))
