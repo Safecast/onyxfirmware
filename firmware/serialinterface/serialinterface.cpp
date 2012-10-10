d1600e0e957c6a1f0d15d4fa15e5818147e6c9fb
bunnie <bunnie@bunniestudios.com>
2012-10-11 01:28:35 +0800

    fix some merge issue

--- firmware/serialinterface/serialinterface.cpp
+++ firmware/serialinterface/serialinterface.cpp
@@ -303,7 +303,6 @@ void serial_process_command(char *line) {
   if(strcmp(line,"WRITEPRIVATEKEY") == 0) {
     serial_writeprivatekey();
   } else 
-<<<<<<< .merge_file_YZDXWS
   if(strcmp(line,"MAGREAD") == 0) {
     gpio_set_mode (PIN_MAP[41].gpio_device,PIN_MAP[41].gpio_bit, GPIO_OUTPUT_PP); // MAGPOWER
     gpio_set_mode (PIN_MAP[29].gpio_device,PIN_MAP[29].gpio_bit, GPIO_INPUT_PU);  // MAGSENSE
