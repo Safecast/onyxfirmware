955a10853d2f90015633c6f7e29abcfd9f2c6a20
Nava Whiteford <new@sgenomics.co.uk>
2012-10-25 12:46:40 +0100

    updated conversion coeff with value from Lee.

--- firmware/geiger/Geiger.cpp
+++ firmware/geiger/Geiger.cpp
@@ -267,7 +267,7 @@ float Geiger::get_microrems() {
 }
 
 float Geiger::get_microsieverts() {
-  float conversionCoefficient = 0.0029;
+  float conversionCoefficient = 0.00294;
   float microsieverts =  (get_cpm_deadtime_compensated() * conversionCoefficient) * calibration_scaling;
   char t[50];
   float_to_char(microsieverts,t,6);
