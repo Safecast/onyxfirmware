8dd355808ad69627c98be5b0fd81e1f03d198336
Nava Whiteford <new@sgenomics.co.uk>
2012-12-26 12:08:30 +0000

    modem is transmitting data to SoftModem app, to some degree, but not decoded correctly.

--- firmware/libmaple/dac.c
+++ firmware/libmaple/dac.c
@@ -80,7 +80,9 @@ void dac_write_channel(const dac_dev *dev, uint8 channel, uint16 val) {
         dev->regs->DHR8R1 = (uint32) val ; //DAC_DHR12R1_DACC1DHR & val;
         break;
     case 2:
-        dev->regs->DHR8R2 = (uint32) val ; //DAC_DHR12R2_DACC2DHR & val;
+     //   dev->regs->DHR8R2 = (uint32) val ; //DAC_DHR12R2_DACC2DHR & val;
+     dev->regs->DHR8R2 = (uint32) val ; //DAC_DHR12R2_DACC2DHR & val;
+     //dev->regs->DHR12R2 = ((uint32) val) << 4;
         break;
     }
 }
@@ -103,7 +105,7 @@ void dac_enable_channel(const dac_dev *dev, uint8 channel) {
         break;
     case 2:
         gpio_set_mode(GPIOA, 5, GPIO_INPUT_ANALOG);
-      // dev->regs->CR |= (DAC_CR_EN2 | DAC_CR_BOFF2);
+        //dev->regs->CR |= (DAC_CR_EN2 | DAC_CR_BOFF2);
         dev->regs->CR |= DAC_CR_EN2;
         break;
     }
