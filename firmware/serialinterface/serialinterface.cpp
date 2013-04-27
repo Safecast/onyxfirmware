56aa643956150668d2cd2e24eb934a08173daf1c
Kalin KOZHUHAROV <me.kalin@gmail.com>
2013-04-27 10:19:17 +0900

    fixing issue #26: KEYVALSET does not "end"

--- firmware/serialinterface/serialinterface.cpp
+++ firmware/serialinterface/serialinterface.cpp
@@ -396,7 +396,10 @@ void serial_setkeyval_run(char *line) {
   }
   
   if(eqpos==-1) return;
-
+  if(eqpos==0) {		// = on a line ends the command
+    command_stack_pop();
+    return;
+  }
   for(int n=0;n<eqpos;n++) {
     key[n]=line[n];
     key[n+1]=0;
@@ -412,7 +415,7 @@ void serial_setkeyval_run(char *line) {
 }
 
 void cmd_keyvalset(char *line) {
-  serial_write_string("KEY=VAL\r\n");
+  serial_write_string("|| Enter KEY=VAL, end with a single = on a new line.\r\n");
   serial_write_string("#>");
 
   command_stack[command_stack_size] = serial_setkeyval_run;
