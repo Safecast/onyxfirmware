6d7f60318b473e8d3d95cb1c89e3eb3aff62e660
Nava Whiteford <new@sgenomics.co.uk>
2013-05-05 15:34:17 +0100

    Re-enabled audio modem issue #40

--- firmware/gui/screen_layout.cpp
+++ firmware/gui/screen_layout.cpp
@@ -31,15 +31,15 @@ __FLASH__ screen screens_layout[SCREEN_COUNT] = {
 
 // Screen 2 - Advanced menu
 {
-  5,
+  6,
   {
     { ITEM_TYPE_HEAD       , 0, 0, "CPMDEADINT"     ,255 }, 
     { ITEM_TYPE_MENU       , 4, 1, "Graphs"         ,1   },
     { ITEM_TYPE_MENU       ,15, 2, "Accumulate/Avg" ,6   },
     { ITEM_TYPE_MENU       ,21, 3, "Becquerel"      ,31  },
 //    { ITEM_TYPE_MENU_ACTION, 0, 4, "QR Transfer"    ,7   },
-    { ITEM_TYPE_MENU_ACTION, 0, 4, "QR Tweet"       ,32  }
-//    { ITEM_TYPE_MENU_ACTION, 0, 6, "Audio Xfer Mod" ,255 },
+    { ITEM_TYPE_MENU_ACTION, 0, 4, "QR Tweet"       ,32  },
+    { ITEM_TYPE_MENU_ACTION, 0, 6, "Audio Xfer"     ,255 }
 //    { ITEM_TYPE_MENU_ACTION, 0, 7, "Audio Xfer Full",255 }
   }
   ,7
