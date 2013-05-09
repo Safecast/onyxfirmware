812d176d06e425b31569cd92f08ae552f313ff43
Edouard Lafargue <edouard@lafargue.name>
2013-05-09 15:27:49 -0700

    Fix JSON log download typo broken by last commit
    The JSON blob ended with a ',' and not a '}'. Fixed now.

--- firmware/log_read/log_read.cpp
+++ firmware/log_read/log_read.cpp
@@ -81,7 +81,7 @@ int log_read_block(char *buf) {
    // buf += strlen(buf);
     sprintf(buf,"\"accel_x_start\":%d,\"accel_y_start\":%d,\"accel_z_start\":%d,",flash_log[log_position].accel_x_start,flash_log[log_position].accel_y_start,flash_log[log_position].accel_z_start);
     buf += strlen(buf);
-    sprintf(buf,"\"accel_x_end\":%d,\"accel_y_end\":%d,\"accel_z_end\":%d,",flash_log[log_position].accel_x_end,flash_log[log_position].accel_y_end,flash_log[log_position].accel_z_end);
+    sprintf(buf,"\"accel_x_end\":%d,\"accel_y_end\":%d,\"accel_z_end\":%d}",flash_log[log_position].accel_x_end,flash_log[log_position].accel_y_end,flash_log[log_position].accel_z_end);
     buf += strlen(buf);
    // sprintf(buf,"\"magsensor_start\":%d,\"magsensor_end\":%d}",flash_log[log_position].magsensor_start,flash_log[log_position].magsensor_end);
    // buf += strlen(buf);
